5e040ac166ede7b1f482843da2c9e6262773df39
shlongisdookielol <73070229+shlongisdookielol@users.noreply.github.com>
2021-07-07 10:48:48 -0500

    warinspace bots (copies packets from client)

---
+++ warinspace bots.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         A A Warinspace Bots
+// @version      2
+// @author       enderman8686#2873
+// @match        http://warin.space/
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addValueChangeListener
+// ==/UserScript==
+
+(() => {
+    "use strict";
+    let server;
+    let toggle = false;
+    const websockets = WebSocket.prototype.send;
+
+    WebSocket.prototype.send = function(data) {
+        console.log(new Uint8Array(data))
+        server = this.url;
+        GM_setValue("WsPackets", Array.from(new Uint8Array(data)));
+        return websockets.call(this, data);
+    }
+
+    setInterval(() => {
+        if (toggle) {
+            let websocket = new WebSocket(server);
+            websocket.onopen = () => {
+                websocket.send(new Uint8Array([5]));
+                websocket.send(new Uint8Array([4, 2, 0]));
+                GM_addValueChangeListener("WsPackets", (prostate /*literally useless and doesnt do anything but okay*/) => {
+                    websocket.send(new Uint8Array(GM_getValue("WsPackets")));
+                });
+            }
+        }
+    }, 1000);
+
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode == 84) {
+            toggle = !toggle
+        }
+    });
+})();
