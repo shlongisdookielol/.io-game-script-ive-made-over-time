e924739b50c512e8f374f7d759780d3ab7c3a2a6
shlongisdookielol <73070229+shlongisdookielol@users.noreply.github.com>
2021-07-07 10:53:12 -0500

    Create Arras.io multibox script.js

---
+++ Arras.io multibox script.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Arras.io multibox script
+// @namespace    http://tampermonkey.net/k
+// @version      emo logan paul
+// @author       enderman8686#2873
+// @match        *://arras.io/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addValueChangeListener
+// ==/UserScript==
+//sdfjhsdkjfhjsdakjfhsadkjfhsdakjfhsadf
+
+(() => {
+    "use strict";
+    // multibox stuff
+    const websockets = WebSocket.prototype.send;
+    let ws, packet;
+    let main = false, initiation = false;
+
+    WebSocket.prototype.send = function (emologanpaul) {
+        if (main) {
+            GM_setValue("WSVal", Array.from(new Uint8Array(emologanpaul)));
+        }
+        ws = this;
+        return websockets.call(this, emologanpaul);
+    };
+
+    GM_addValueChangeListener("WSVal", (prostate) => {
+        initiation = GM_getValue("SendToAll");
+        packet = GM_getValue("WSVal");
+        if (initiation && !main && GM_getValue("SendToAll") !== null) {
+            ws.send(new Uint8Array(packet));
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode == 70) {
+            main = !main;
+            console.log("%c [F] Main Window: " + main, "color: #f700ff; font-size: 2em;");
+        }
+        if (e.keyCode == 84) {
+            GM_setValue("SendToAll", initiation = !initiation);
+            console.log("%c [T] Multibox: " + initiation, "color: #00ff2f; font-size: 2em;");
+        }
+    });
+})();
