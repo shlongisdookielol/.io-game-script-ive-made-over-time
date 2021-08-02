9963d78d8e3321b67dc2cf3018ce16c9b440e8ed
shlongisdookielol <73070229+shlongisdookielol@users.noreply.github.com>
2021-08-02 12:49:33 -0500

    Update Diep.io Glow.js

--- Diep.io Glow.js
+++ Diep.io Glow.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Diep.io Glow Theme
 // @namespace    http://tampermonkey.net/LIGMABALLS
-// @version      2
+// @version      3
 // @description  lol
 // @author       shlong#2873
 // @match        *://diep.io/
@@ -10,142 +10,73 @@
 
 (() => {
     "use strict";
-
-    const proxiedHook = CanvasRenderingContext2D.prototype;
-    const _fill = CanvasRenderingContext2D.prototype.fill,
-          _rect = CanvasRenderingContext2D.prototype.fillRect,
-          _drawImage = CanvasRenderingContext2D.prototype.drawImage;
+    const proxied = CanvasRenderingContext2D.prototype;
     const canvas = document.getElementById("canvas"),
           ctx = canvas.getContext("2d");
-    const UsernameBar = document.getElementById("textInput")
-    const otherInit = setInterval(() => {
-        if (document.getElementById("loading").innerText == "") {
-            unsafeWindow.input.execute("ren_background_color 0x1a1a1a");
-            clearInterval(otherInit);
-        }
-    });
 
-    // proxied hook stuff
-    proxiedHook.fillText = new Proxy(proxiedHook.fillText, {
-        apply(_fillText, _thisArg, args) {
-            if (args[0] == "You're using an adblocker, please consider disabling it to support the game") {
-                args[0] = " Thank you for using my theme, if you need help just contact my discord :).";
-                _thisArg.fillStyle = "red";
-                _thisArg.shadowColor = "red";
-                _thisArg.shadowBlur = 20;
+    window = unsafeWindow;
+
+    proxied.fillRect = new Proxy(proxied.fillRect, {
+        apply(type, _this, args) {
+            if (["#8efffb",
+                 "#71ccc8",
+                 "#a4fffb",
+                 "#b4ff8e",
+                 "#90cc71",
+                 "#c3ffa4",
+                 "#ff8e8e",
+                 "#cc7171",
+                 "#ffa4a4",
+                 "#ffeb8e",
+                 "#ccbc71",
+                 "#ffefa4",
+                 "#8eb2ff",
+                 "#718ecc",
+                 "#a4c1ff",
+                 "#b58eff",
+                 "#9071cc",
+                 "#c3a4ff",
+                 "#fb8eff",
+                 "#c871cc",
+                 "#fba4ff",
+                 "#fdcdac",
+                 "#caa489",
+                 "#fdd7bc",
+                ].includes(_this.fillStyle)) {
+                _this.fillStyle = "transparent";
             }
-            _fillText.apply(_thisArg, args);
-        }
+            type.apply(_this, args);
+        },
     });
 
-    proxiedHook.fillRect = new Proxy(proxiedHook.fillRect, { // thanks shadam for this
-        apply(_fillRect, _thisArg, args) {
-            if([
-                // FFA
-                "#8efffb",
-                "#71ccc8",
-                "#a4fffb",
-                // SURVIVAL
-                "#b4ff8e",
-                "#90cc71",
-                "#c3ffa4",
-                // 2 TEAMS
-                "#ff8e8e",
-                "#cc7171",
-                "#ffa4a4",
-                // 4 TEAMS
-                "#ffeb8e",
-                "#ccbc71",
-                "#ffefa4",
-                // DOMINATION
-                "#8eb2ff",
-                "#718ecc",
-                "#a4c1ff",
-                // TAG
-                "#b58eff",
-                "#9071cc",
-                "#c3a4ff",
-                // MAZE
-                "#fb8eff",
-                "#c871cc",
-                "#fba4ff",
-                // SANDBOX
-                "#fdcdac",
-                "#caa489",
-                "#fdd7bc"
-            ].includes(_thisArg.fillStyle)) return;
-            _thisArg.shadowColor = "white";
-            _thisArg.shadowBlur = 20;
-            _fillRect.apply(_thisArg, args);
-        }
-    });
 
-    proxiedHook.strokeText = new Proxy(proxiedHook.strokeText, {
-        apply(_strokeText, _thisArg, args) {
-            _thisArg.strokeStyle = "#383838";
-            _strokeText.apply(_thisArg, args);
-        }
-    });
-
-    proxiedHook.fillRect = new Proxy(proxiedHook.fillRect, {
-        apply(_rect, _thisArg, args) {
-            if (_thisArg.globalAlpha == 0.5) {
-                _thisArg.fillStyle = "#000000";
+    proxied.fillRect = new Proxy(proxied.fillRect, {
+        apply(type, _this, args) {
+            if (_this.fillStyle == "#cdcdcd") {
+                _this.fillStyle = "#1f1f1f";
             }
-            _rect.apply(_thisArg, args);
-        }
+            return type.apply(_this, args);
+        },
     });
 
-    // not proxied
-    CanvasRenderingContext2D.prototype.drawImage = function (image) {
-        if (image.src == "https://static.diep.io/title.png") {
-            image.src = "black"
-        }
-        _drawImage.apply(this, arguments);
-    };
-    CanvasRenderingContext2D.prototype.fill = function () {
-        // turret
-        if (this.fillStyle == "#999999") {
-            this.fillStyle = "transparent";
-            this.shadowColor = "white";
-            this.shadowBlur = 20;
-            // teams
-        } else if (this.fillStyle == "#00b2e1") { // blue team
-            this.shadowColor = "#00b2e1";
-            this.shadowBlur = 20;
-        } else if (this.fillStyle == "#f14e54") { // red team
-            this.shadowColor = "#f14e54";
-            this.shadowBlur = 20;
-        } else if (this.fillStyle == "#00e16e") { // green team
-            this.shadowColor = "#00e16e";
-            this.shadowBlur = 20;
-        } else if (this.fillStyle == "#bf7ff5") { // purple team
-            this.shadowColor = "#bf7ff5";
-            this.shadowBlur = 20;
+    proxied.stroke = new Proxy(proxied.stroke, {
+        apply(type, _this, args) {
+            _this.lineWidth = 3.5;
+            _this.shadowColor = _this.strokeStyle;
+            _this.shadowBlur = 13.5;
+            return type.apply(_this, args);
+        },
+    });
 
-            // shapes
-        } else if (this.fillStyle == "#ffe869") { // square
-            this.fillStyle = "transparent";
-            this.shadowColor = "#ffe869";
-            this.shadowBlur = 15;
-        } else if (this.fillStyle == "#fc7677") { // triangle
-            this.fillStyle = "transparent";
-            this.shadowColor = "#fc7677";
-            this.shadowBlur = 15;
-        } else if (this.fillStyle == "#768dfc") { // pentagon
-            this.fillStyle = "transparent";
-            this.shadowColor = "#768dfc";
-            this.shadowBlur = 15;
-        } else if (this.fillStyle == "#f177dd") { // crashers
-            this.fillStyle = "transparent";
-            this.shadowColor = "#f177dd";
-            this.shadowBlur = 15;
-
-        } else if (this.fillStyle == "#000000") { // crashers
-            this.fillStyle = "white"
-            this.shadowColor = this.fillStyle;
-            this.shadowBlur = 15;
-        }
-        _fill.apply(this, arguments);
-    };
+    proxied.fill = new Proxy(proxied.fill, {
+        apply(type, _this, args) {
+            if (_this.fillStyle == "#cdcdcd") {
+                _this.fillStyle = "#36393f";
+            }
+            _this.strokeStyle = _this.fillStyle;
+            _this.stroke();
+            _this.fillStyle = "transparent";
+            return type.apply(_this, args);
+        },
+    });
 })();
